9a35e6c66198ecfba8e39ca0db1d921bd649f480
asirshaikh49 <asirshaikh49@gmail.com>
2022-08-04 17:43:48 +0530

    kkk

--- app.py
+++ app.py
@@ -149,5 +149,5 @@ def logout():
     return redirect("/")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 33507))
-    app.run(debug=True, port=33507)
+
+    app.run(debug=True)
