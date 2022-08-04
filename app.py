d66e05ea470c2fef6b4520b52f6401b8bdea32ab
asirshaikh49 <asirshaikh49@gmail.com>
2022-08-04 17:30:38 +0530

    commit

--- app.py
+++ app.py
@@ -149,4 +149,5 @@ def logout():
     return redirect("/")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True, port=33507)
